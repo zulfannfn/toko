d36bfc0dbc188635fde83e934dbd67f4ec3f43b7
zulfannfn <dzulfannurf@example.com>
2024-02-02 08:13:11 +0700

    Agenda Input Product

---
+++ resources/js/Fragment/FormInput.jsx
@@ -0,0 +1,9 @@
+const FormInput = () => {
+    return(
+        <div>
+            <p>Input Produk</p>
+            <input name="" type="text" />
+        </div>
+    )
+}
+export default FormInput
---
+++ resources/js/Pages/Admin/InputProduct.jsx
@@ -0,0 +1,9 @@
+import FormInput from "@/Fragment/FormInput"
+
+export default function InputProduct() {
+    return(
+        <div>
+            <FormInput/>
+        </div>
+    )
+}
